4f3c41fd044636efac31f6d7352b9455093e373c
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-07-17 14:47:15 +0300

    add return data in IReminderService.cs interface

--- Backend/Reglamentator.Application/Abstractions/IReminderService.cs
+++ Backend/Reglamentator.Application/Abstractions/IReminderService.cs
@@ -6,7 +6,7 @@ namespace Reglamentator.Application.Abstractions;
 
 public interface IReminderService
 {
-    Task<Result> AddReminderAsync(long telegramId, long operationId, CreateReminderDto reminderDto, CancellationToken cancellationToken = default);
-    Task<Result> UpdateReminderAsync(long telegramId, long operationId, UpdateReminderDto reminderDto, CancellationToken cancellationToken = default);
-    Task<Result> DeleteReminderAsync(long telegramId, long operationId, long reminderId, CancellationToken cancellationToken = default);
+    Task<Result<Reminder>> AddReminderAsync(long telegramId, long operationId, CreateReminderDto reminderDto, CancellationToken cancellationToken = default);
+    Task<Result<Reminder>> UpdateReminderAsync(long telegramId, long operationId, UpdateReminderDto reminderDto, CancellationToken cancellationToken = default);
+    Task<Result<Reminder>> DeleteReminderAsync(long telegramId, long operationId, long reminderId, CancellationToken cancellationToken = default);
 }
