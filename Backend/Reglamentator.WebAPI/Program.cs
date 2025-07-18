63e1d402bbef3412bb8626a578ce9355fcff6e6d
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-07-18 15:08:13 +0300

    change Program.cs

--- Backend/Reglamentator.WebAPI/Program.cs
+++ Backend/Reglamentator.WebAPI/Program.cs
@@ -1,3 +1,6 @@
+using Reglamentator.Application.Extensions;
+using Reglamentator.Data.Extensions;
+using Reglamentator.WebAPI.Extensions;
 using Reglamentator.WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,10 +9,16 @@ builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddGrpc();
 
+builder.Services.AddAutoMapperWithConfigure();
+builder.Services.AddApplicationServices();
+builder.Services.AddHangfire(builder.Configuration);
+builder.Services.AddDbContext(builder.Configuration);
+
 var app = builder.Build();
 
 app.MapGrpcService<OperationGrpcService>();
 app.MapGrpcService<UserGrpcService>();
 app.MapGrpcService<NotificationGrpcService>();
+app.MapGrpcService<ReminderGrpcService>();
 
 app.Run();
