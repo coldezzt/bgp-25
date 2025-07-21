154c204bc306af4ef5be4313fa340e96beacf521
DmitriyAlexv <dmitriy_aleksandrov_2004@bk.ru>
2025-07-21 12:43:51 +0300

    remove unnecessary mapping definition for TelegramUser.cs

--- Backend/Reglamentator.WebAPI/Mapping/Profiles/TelegramUserMappingProfile.cs
+++ Backend/Reglamentator.WebAPI/Mapping/Profiles/TelegramUserMappingProfile.cs
@@ -8,11 +8,8 @@ public class TelegramUserMappingProfile: Profile
 {
     public TelegramUserMappingProfile()
     {
-        CreateMap<TelegramUserDto, CreateUserDto>()
-            .ForMember(dest => dest.TelegramId, opt => opt.MapFrom(src => src.TelegramId));
-        
-        CreateMap<TelegramUser, TelegramUserDto>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
-            .ForMember(dest => dest.TelegramId, opt => opt.MapFrom(src => src.TelegramId));
+        CreateMap<TelegramUserDto, CreateUserDto>();
+
+        CreateMap<TelegramUser, TelegramUserDto>();
     }
 }
